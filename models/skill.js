e7f6ecb5d34346b0d14be9ff4ad49b8e3ce3425b
Sungjun Choi <sungjun0110@gmail.com>
2021-12-03 14:38:51 -0800

    add models/skill.js

---
+++ models/skill.js
@@ -0,0 +1,19 @@
+const skill = [
+    {id: 001, skill: 'Javascript', level: 'Good'},
+    {id: 021, skill: 'HTML', level: 'Good'},
+    {id: 321, skill: 'CSS', level: 'Good'},
+    {id: 521, skill: 'jQuery', level: 'Good'},
+];
+
+module.exports = {
+    getAll,
+
+};
+
+function getAll() {
+    return skill;
+}
+
+// function getOne() {
+//     return 
+// }
